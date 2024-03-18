9cb9cb6862e0f202fa814cabcb9aea481fba8267
stumburu <163906495+stumburu@users.noreply.github.com>
2024-03-18 16:02:58 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 # Import python packages
+import requests
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
@@ -10,42 +11,28 @@ st.write(
     """
 )
 
-#option = st.selectbox(
-#   'What is your favourite Fruit?',
-#    ('Bananas', 'StrawBerries', 'Peaches'))
-
-#st.write('Your Favourite Fruit is:', option)
-
 name_on_order = st.text_input('Name on Smoothie')
 st.write('The Name on your Smoothie will be', name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list=st.multiselect( 'Choose up to 5 ingredients:',
 my_dataframe,max_selections = 5)
 
 if ingredients_list:
-    #st.write(ingredients_list)
-    #st.text(ingredients_list)
     ingredients_string = ' '
     for fruit_choosen in ingredients_list:
         ingredients_string += fruit_choosen + ' '
-   # st.write(ingredients_string)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st. dataframe(data=fruityvice_response.json(), use_container_width=True)
 
 my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
-
-#st.write(my_insert_stmt)
 
 time_to_insert = st.button('Submit order')
 if time_to_insert:
     session.sql(my_insert_stmt).collect()
     st.success('Your Smoothie is ordered '+ name_on_order, icon="✅")
-
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_df = st. dataframe(data=fruityvice_response.json(), use_container_width=True)
+    
